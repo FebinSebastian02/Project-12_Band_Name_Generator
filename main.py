a33fe1d4a9c9cb844b3b85d20b8e12fabf7759a9
FebinSebastian <febin.vadakkan@gmail.com>
2024-04-04 00:12:28 +0200

    Committed completed Day1 project

---
+++ main.py
@@ -0,0 +1,7 @@
+#Band name generator uses input and print functions to print a band name
+
+print("Welcome to the Band Generator\n")
+city = input("Enter your city name\n")
+petName = input("Enter your pet name\n")
+print("Your band name could be " + city + " " + petName)
+
